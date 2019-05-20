072def4b0c84514864949026467de12e9ffdc619
Andrew Li <andrewli@andrews-mbp.lan>
2019-05-20 03:22:37 -0700

    same but slightly less ugly

--- app.js
+++ app.js
@@ -9,24 +9,30 @@ function handleSubmit(event){
 }
 
 function makeMemeDiv(url,top_text,bottom_text){
-    let wholeDiv = document.createElement('div')
-    let topDiv = document.createElement('div')
-    let bottomDiv = document.createElement('div')
-    let img = document.createElement('img')
-    let wrapper = document.createElement('div')
+    let wholeDiv = document.createElement('div') //container for entire meme
+    let topDiv = document.createElement('div')  //contains text on top
+    let bottomDiv = document.createElement('div')   //contains text on bottom
+    let img = document.createElement('img') //contains image
+    let shadowContainer = document.createElement('div') //contains shadow img
+    let shadow = document.createElement('img')  //thing that'll show on hover
 
     wholeDiv.className="Meme"
     wholeDiv.id="meme"+counter
     topDiv.className="top_text"
     bottomDiv.className="bottom_text"
+    shadowContainer.className="shadow_container"
+    shadow.className="shadow"
 
     img.src=url
     topDiv.innerHTML=top_text
     bottomDiv.innerHTML=bottom_text
+    shadow.src="x-mark.png"
 
     wholeDiv.append(topDiv)
     wholeDiv.append(img)
     wholeDiv.append(bottomDiv)
+    wholeDiv.append(shadowContainer)
+    shadowContainer.append(shadow)
     return wholeDiv
 }
 
